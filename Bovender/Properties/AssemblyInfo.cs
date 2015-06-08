a800ce94f5e179dc4fe8accffceaf3c925e6167f
Daniel Kraus <krada@gmx.net>
2015-06-08 05:34:47 +0200

    Edit Bovender project information.

--- Bovender/Properties/AssemblyInfo.cs
+++ Bovender/Properties/AssemblyInfo.cs
@@ -23,11 +23,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Bovender")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("In-house framework")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Daniel Kraus")]
 [assembly: AssemblyProduct("Bovender")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
+[assembly: AssemblyCopyright("Copyright © Daniel Kraus 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -49,5 +49,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
