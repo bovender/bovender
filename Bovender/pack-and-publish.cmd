ef0e7b67795e97b56989f425629f957f902b0a62
Daniel Kraus <bovender@bovender.de>
2016-05-24 17:20:54 +0200

    Pause at the end of pack-and-publish.cmd.

--- Bovender/pack-and-publish.cmd
+++ Bovender/pack-and-publish.cmd
@@ -1,3 +1,4 @@
 REM nuget pack Bovender.csproj -Build -Symbols -Properties Configuration=Release
 nuget pack Bovender.csproj -Build -Symbols -Properties Configuration=Debug
 nuget push Bovender*.nupkg
+pause
