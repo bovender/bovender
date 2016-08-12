8439bfa82a3fa29f2e2eb80141768c58c133dfe6
Daniel Kraus <bovender@bovender.de>
2016-08-12 06:47:00 +0200

    Add VisibilityBooleanNegationConverter.
    - NEW: Added a VisibilityBooleanNegationConverter.

---
+++ Bovender/Mvvm/Converters/VisibilityBooleanNegationConverter.cs
@@ -0,0 +1,51 @@
+﻿/* VisibilityBooleanConverter - Copy.cs
+ * part of Bovender framework
+ * 
+ * Copyright 2014-2016 Daniel Kraus
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using System;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Bovender.Mvvm.Converters
+{
+    public class VisibilityBooleanNegationConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if ((bool)value == true)
+            {
+                return Visibility.Collapsed;
+            }
+            else
+            {
+                return Visibility.Visible;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            switch ((Visibility)value)
+            {
+                case Visibility.Visible: return false;
+                case Visibility.Hidden: return true;
+                case Visibility.Collapsed: return true;
+                default:
+                    throw new ArgumentException(
+                        "No conversion defined for " + ((Visibility)value).ToString());
+            }
+        }
+    }
+}
