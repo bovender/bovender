f77ae5f9a1231cf0043d4b70c94454b438fd7b9e
Daniel Kraus <bovender@bovender.de>
2016-08-19 19:27:49 +0200

    Add ReleaseComObject extension method to object.
    - NEW: ReleaseComObject extension method for object.

--- Bovender/Extensions/ObjectExtensions.cs
+++ Bovender/Extensions/ObjectExtensions.cs
@@ -18,6 +18,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace Bovender.Extensions
@@ -27,6 +28,40 @@ namespace Bovender.Extensions
         public static string ComputeMD5Hash(this object obj)
         {
             return CommonHelpers.ComputeMD5Hash(obj);
+        }
+
+        /// <summary>
+        /// If the object is not null and has an underlying runtime-callable wrapper (RCW) for
+        /// a COM object, Marshal.ReleaseComObject is called with the object.
+        /// </summary>
+        /// <param name="obj">Object whose COM association to release. This must not be a
+        /// dynamic type!</param>
+        /// <returns>Null if the COM object was released or if the object was null, or the object 
+        /// itself if it does not have an underlying COM object.</returns>
+        public static object ReleaseComObject(this object obj)
+        {
+            if (obj != null && Marshal.IsComObject(obj))
+            {
+                Marshal.ReleaseComObject(obj);
+                return null;
+            }
+            else
+            {
+                return obj;
+            }
+        }
+
+        public static dynamic ReleaseDynamicComObject(dynamic obj)
+        {
+            if (obj != null && Marshal.IsComObject(obj))
+            {
+                Marshal.ReleaseComObject(obj);
+                return null;
+            }
+            else
+            {
+                return obj;
+            }
         }
     }
 }
